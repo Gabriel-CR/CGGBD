641215fb6ca0d37135032662f82f31df11b39b52
Gabriel Castro <gabriel.cgcr.cgcr@gmail.com>
2023-05-22 15:19:16 -0300

    add consultas

---
+++ consultas.sql
@@ -0,0 +1,9 @@
+-- Temperatura máxima em cada loca
+--  usar latitude e longitude
+--  usar gráfico do brasil
+
+-- Informações de um determinado local
+
+-- Informações de um determinado local em um determinado periodo
+
+-- Temperatura mais alta do estado
