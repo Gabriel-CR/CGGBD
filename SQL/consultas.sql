4bf58ea06823e0fa2a3ac72804f8574a0ced4cbd
Gabriel Castro <gabriel.cgcr.cgcr@gmail.com>
2023-06-01 17:05:49 -0300

    add consulta

--- SQL/consultas.sql
+++ SQL/consultas.sql
@@ -6,8 +6,15 @@
 
 -- Informações de um determinado local em um determinado periodo
 
--- Temperatura mais alta do estado 
+-- Temperatura mais alta do estado
+SELECT	ST.state, (
+	SELECT	MAX(I.tmax)
+	FROM	station AS S JOIN info_temperatura AS I ON S.id = I.id_station
+	WHERE	ST.state = S.state
+) AS temperatura
+FROM	station AS ST
+GROUP BY ST.state
+ORDER BY temperatura;
 
 -- Quais os meses com maiores temperaturas em cada estado
-
 
