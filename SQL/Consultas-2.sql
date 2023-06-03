6e94e3dddb555f6a506c8669152a4ffebf59f57e
Gabriel Castro <gabriel.cgcr.cgcr@gmail.com>
2023-06-03 17:49:05 -0300

    alter consulta 2

--- SQL/Consultas-2.sql
+++ SQL/Consultas-2.sql
@@ -24,13 +24,13 @@ GROUP BY nh.wsmn, nh.data;
 
 -- Listar as cidades com valor elevação ordenado em ordem descrecente com sua temperatura max e min 
 
-SELECT nh.wsnm, nh.elvt, max(nh.tmax), min(nh.tmin), nh.data
-FROM station as st join north_heast as nh on st.region = nh.region
-WHERE nh.prov = 'CE' and
+SELECT st.station, st.height, max(nh.tmax), min(nh.tmin), nh.data
+FROM station as st join info_temperatura as nh on st.id = nh.id_station
+WHERE st.state = 'CE' and
 	nh.data between '2021-01-01' and '2022-12-31' and
 	nh.tmin <> -9999
-GROUP BY nh.wsnm, nh.elvt, nh.data
-ORDER BY nh.elvt DESC;
+GROUP BY st.station, st.height, nh.data
+ORDER BY st.height DESC;
 
 -- Listar as cidades no CE que tiveram a temperatura de 35 graus ou mais e quantas vezes aconteceu 
 
